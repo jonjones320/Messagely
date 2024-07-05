d94bc14d22988ba71ade7ab0cfd3986a26b6ae92
JJ <jonjones320@gmail.com>
2024-07-05 05:29:32 -0700

    Added 'dotenv' library to provide environment variables

--- config.js
+++ config.js
@@ -2,11 +2,15 @@
 
 // read .env files and make environmental variables
 
-require("dotenv").config();
+require('dotenv').config();
+
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const dbname = process.env.DB_NAME;
 
 const DB_URI = (process.env.NODE_ENV === "test")
-  ? "postgresql:///messagely_test"
-  : "postgresql:///messagely";
+  ? `postgresql://${username}:${password}@localhost:5432/${dbname}_test`
+  : `postgresql://${username}:${password}@localhost:5432/${dbname}`;
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
 
--- db.js
+++ db.js
@@ -4,9 +4,16 @@
 const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
-const client = new Client(DB_URI);
+// const client = new Client(DB_URI);
 
-client.connect();
+// client.connect();
 
+let db = new Client({
+    connectionString: DB_URI
+  });
+  
+db.connect()
+    .then(() => console.log('Connected to the database'))
+    .catch(err => console.error('Connection error', err));;
 
-module.exports = client;
+module.exports = db;
