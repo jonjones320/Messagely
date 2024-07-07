59d2b72e23350c7c95836da197f1e54f3d334782
JJ <jonjones320@gmail.com>
2024-07-06 21:46:43 -0700

    Created message routes for creating a new message, viewing a message, and marking a message as 'read'.

--- routes/messages.js
+++ routes/messages.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const router = new express.Router();
+const ExpressError = require("../expressError");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+const Message = require("../models/message");
+
+
+
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -10,6 +18,17 @@
  * Make sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get('/:id', ensureCorrectUser, async (req, res, next) => {
+    try {
+        const message = await Message.get(req.param.id);
+        return res.json({ message });
+    }
+    catch(error) {
+        return next(error);
+    }
+});
+
+
 
 
 /** POST / - post message.
@@ -18,6 +37,17 @@
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post('/:from_username/to_username/body', ensureCorrectUser, async (req, res, next) => {
+    try {
+        const newMessage = await Message.create(req.params[from_username, to_username, body]);
+        return res.json({ newMessage });
+    }
+    catch(error) {
+        return next(error);
+    }
+});
+
+
 
 
 /** POST/:id/read - mark message as read:
@@ -27,4 +57,16 @@
  * Make sure that the only the intended recipient can mark as read.
  *
  **/
+router.get('/:id', ensureCorrectUser, async (req, res, next) => {
+    try {
+        await Message.markRead(req.params.id);
+        return res.json( message, "Success")
+    }
+    catch(error) {
+        return next(error);
+    }
+});
 
+
+
+module.exports = router;
